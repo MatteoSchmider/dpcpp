810988005cb7325037f0c61831080ad6baedb484
Matteo Schmider <matteo.schmider@student.kit.edu>
2024-08-14 19:56:35 +0200

    test artifact path error

--- build/win/opencl.bat
+++ build/win/opencl.bat
@@ -1,13 +1,15 @@
 :: install ninja
-choco install ninja -y
-
-:: set compiler paths
-call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
-
-:: download dpc++ sources into C:\dpcpp
-git clone --config core.autocrlf=false https://github.com/intel/llvm -b sycl
-move "llvm" "dpcpp"
-
-:: configure and compile dpc++
-python "dpcpp\buildbot\configure.py" -t Release
-python "dpcpp\buildbot\compile.py" -j 16
+:: choco install ninja -y
+:: 
+:: :: set compiler paths
+:: call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+:: 
+:: :: download dpc++ sources into C:\dpcpp
+:: git clone --config core.autocrlf=false https://github.com/intel/llvm -b sycl
+:: move "llvm" "dpcpp"
+:: 
+:: :: configure and compile dpc++
+:: python "dpcpp\buildbot\configure.py" -t Release
+:: python "dpcpp\buildbot\compile.py" -j 16
+mkdir dpcpp
+mkdir dpcpp/build
