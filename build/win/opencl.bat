b6eec4b5ecfbd461d1dbc6e8bccaeae1acd4ba70
Matteo Schmider <matteo.schmider@student.kit.edu>
2024-08-14 20:00:12 +0200

    testing

--- build/win/opencl.bat
+++ build/win/opencl.bat
@@ -1,16 +1,13 @@
 :: install ninja
-:: choco install ninja -y
-:: 
-:: :: set compiler paths
-:: call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
-:: 
-:: :: download dpc++ sources into C:\dpcpp
-:: git clone --config core.autocrlf=false https://github.com/intel/llvm -b sycl
-:: move "llvm" "dpcpp"
-:: 
-:: :: configure and compile dpc++
-:: python "dpcpp\buildbot\configure.py" -t Release
+choco install ninja -y
+
+:: set compiler paths
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+
+:: download dpc++ sources into C:\dpcpp
+git clone --config core.autocrlf=false https://github.com/intel/llvm -b sycl
+move "llvm" "dpcpp"
+
+:: configure and compile dpc++
+python "dpcpp\buildbot\configure.py" -t Release
 :: python "dpcpp\buildbot\compile.py" -j 16
-mkdir dpcpp
-cd dpcpp
-mkdir build
